6538d06beec262ad8669497358f3387c38a3ed3f
bonjour <>
2020-12-25 15:38:53 +0800

    添加测试文件

--- pkg/framework/plugin/middleware/middleware_gin/operator.go
+++ pkg/framework/plugin/middleware/middleware_gin/operator.go
@@ -1,14 +1,14 @@
 package middleware_gin
 
-
 type operatorTest struct {
-	UserId uint64
-	Username string
+	UserId    uint64
+	Username  string
+	ContextId string
 }
 
 func (c *operatorTest) SetContextId(uuid string) {
-	
+	c.ContextId = uuid
 }
 func (c *operatorTest) GetContextId() string {
-
+	return c.ContextId
 }
--- pkg/framework/plugin/middleware/middleware_gin/plugin_test.go
+++ pkg/framework/plugin/middleware/middleware_gin/plugin_test.go
@@ -1,3 +1,37 @@
 package middleware_gin
 
+import (
+	"fmt"
+	"framework/class/middleware"
+	"testing"
+)
 
+var pluginTest middleware.OperatorContext
+
+func TestPlugin_Init(t *testing.T) {
+	pluginTest = New(
+		WithModel(&operatorTest{}),
+		WithCipherKey([]byte("123123")),
+	)
+
+	opInfo := &operatorTest{
+		UserId:    1,
+		Username:  "gaoshou",
+		ContextId: "",
+	}
+
+	str, err := pluginTest.SignedString(opInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("string:", str)
+
+	{
+		opInfo := &operatorTest{}
+		p := pluginTest.(*plugin)
+		if err := p.decrypt(p.cipherKey,str,opInfo); err != nil {
+			panic(err)
+		}
+		fmt.Println(opInfo)
+	}
+}
