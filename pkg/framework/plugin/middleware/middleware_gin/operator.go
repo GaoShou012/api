829b4bba25b724a89fe9845be59ed1721238bac7
bonjour <>
2020-12-25 15:22:21 +0800

    fix:删除go.mod

---
+++ pkg/framework/plugin/middleware/middleware_gin/operator.go
@@ -0,0 +1,14 @@
+package middleware_gin
+
+
+type operatorTest struct {
+	UserId uint64
+	Username string
+}
+
+func (c *operatorTest) SetContextId(uuid string) {
+	
+}
+func (c *operatorTest) GetContextId() string {
+
+}
