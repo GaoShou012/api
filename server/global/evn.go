5b035c24b51a14a8d4af3ea8c0d0b26992bce987
bonjour <>
2020-12-25 16:40:10 +0800

    fix:修复framework go.mod文件，并且过滤 go.sum文件

--- server/global/evn.go
+++ server/global/evn.go
@@ -1,18 +1,12 @@
 package global
 
 import (
-	"framework/class/middleware"
-	"github.com/casbin/casbin/v2"
 	"github.com/go-redis/redis/v8"
 	"github.com/jinzhu/gorm"
 )
 
 var (
-	CasbinEnforcer *casbin.Enforcer
 	RedisClient    *redis.Client
-	//RedisClient *redis.ClusterClient
-	DBMaster *gorm.DB
-	DBSlave  *gorm.DB
-
-	OperatorContext middleware.OperatorContext
+	DBMaster       *gorm.DB
+	DBSlave        *gorm.DB
 )
