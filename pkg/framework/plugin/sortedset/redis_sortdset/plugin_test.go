43c372f877bd22478c1cdcc91612e56dbd6eadf9
jax <627137323@qq.com>
2021-01-07 16:39:20 +0800

    no message

--- pkg/framework/plugin/sortedset/redis_sortdset/plugin_test.go
+++ pkg/framework/plugin/sortedset/redis_sortdset/plugin_test.go
@@ -1,10 +1,9 @@
 package redis_sortdset
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPlugin_Exists(t *testing.T) {
-	dns := fmt.Sprintf("redis_sortdset://:@127.0.0.1:17001?Db=0&PoolMax=100&PoolMin=10")
+	//dns := fmt.Sprintf("redis_sortdset://:@127.0.0.1:17001?Db=0&PoolMax=100&PoolMin=10")
 }
