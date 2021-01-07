c4dcfe59ea784da0f96a2351a60ffc39c6af5db1
jax <627137323@qq.com>
2021-01-07 15:44:57 +0800

    sortedset

--- pkg/framework/plugin/sortedset/redis/plugin.go
+++ pkg/framework/plugin/sortedset/redis/plugin.go
@@ -12,7 +12,7 @@ type plugin struct {
 }
 
 func (p *plugin) Exists(topic string) (bool, error) {
-	num,err := p.opts.redisClient.ZCard(topic).Result()
+	num, err := p.opts.redisClient.ZCard(topic).Result()
 	if err != nil {
 		return false, err
 	}
@@ -23,7 +23,7 @@ func (p *plugin) Exists(topic string) (bool, error) {
 }
 
 func (p *plugin) Len(topic string) int64 {
-	num,err := p.opts.redisClient.ZCard(topic).Result()
+	num, err := p.opts.redisClient.ZCard(topic).Result()
 	if err != nil {
 		panic(err)
 	}
@@ -31,27 +31,26 @@ func (p *plugin) Len(topic string) int64 {
 }
 
 func (p *plugin) Find(topic string, page int64, pageSize int64) ([]sortedset.Item, error) {
-	members,err := p.opts.redisClient.ZRange(topic,page,page*pageSize).Result()
+	members, err := p.opts.redisClient.ZRange(topic, page, page*pageSize).Result()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-
 
 	i := 0
 	items := make([]sortedset.Item, len(members))
 	for v, k := range members {
 		evt := &item{
-			iKey:k,
-			iVal:v,
+			iKey: k,
+			iVal: v,
 		}
 		items[i] = evt
 		i++
 	}
-	return items,nil
+	return items, nil
 }
 
 func (p *plugin) SetItem(topic string, key string, val float64) error {
-	_,err := p.opts.redisClient.ZAdd(topic,redis.Z{
+	_, err := p.opts.redisClient.ZAdd(topic, redis.Z{
 		Score:  val,
 		Member: key,
 	}).Result()
@@ -62,30 +61,74 @@ func (p *plugin) SetItem(topic string, key string, val float64) error {
 }
 
 func (p *plugin) GetOffset(topic string, key string) (int64, error) {
-	panic("implement me")
+	index, err := p.opts.redisClient.ZRank(topic, key).Result()
+	if err != nil {
+		return 0, err
+	}
+	return index, nil
 }
 
 func (p *plugin) GetOffsetN(topic string, key string) (int64, error) {
-	panic("implement me")
+
+	all, err := p.opts.redisClient.ZLexCount(topic, "-", "+").Result()
+	if err != nil {
+		return 0, err
+	}
+	if all == 0 {
+		return 0, nil
+	}
+	index, err := p.opts.redisClient.ZRank(topic, key).Result()
+	if err != nil {
+		return 0, err
+	}
+	return all - index, nil
 }
 
-func (p *plugin) GetItemFormPositive(topic string) (sortedset.Item, error) {
-	panic("implement me")
+func (p *plugin) GetItemFormPositive(topic string) ([]sortedset.Item, error) {
+	res, err := p.opts.redisClient.ZRange(topic, 0,-1).Result()
+	if err != nil {
+		return nil, err
+	}
+	i := 0
+	items := make([]sortedset.Item, len(res))
+	for v, k := range res {
+		evt := &item{
+			iKey: k,
+			iVal: v,
+		}
+		items[i] = evt
+		i++
+	}
+	return items,nil
 }
 
-func (p *plugin) GetItemFromNegative(topic string) (sortedset.Item, error) {
-	panic("implement me")
+func (p *plugin) GetItemFromNegative(topic string) ([]sortedset.Item, error) {
+	res, err := p.opts.redisClient.ZRevRange(topic, 0,-1).Result()
+	if err != nil {
+		return nil, err
+	}
+	i := 0
+	items := make([]sortedset.Item, len(res))
+	for v, k := range res {
+		evt := &item{
+			iKey: k,
+			iVal: v,
+		}
+		items[i] = evt
+		i++
+	}
+	return items,nil
 }
 
 func (p *plugin) ExistsItem(topic string, key string) (bool, error) {
-	num,err := p.opts.redisClient.ZScore(topic,key).Result()
-	if err != nil{
-		return false,err
+	num, err := p.opts.redisClient.ZScore(topic, key).Result()
+	if err != nil {
+		return false, err
 	}
-	if num == 0{
-		return false,nil
+	if num == 0 {
+		return false, nil
 	}
-	return true,nil
+	return true, nil
 }
 
 func (p *plugin) Init() error {
