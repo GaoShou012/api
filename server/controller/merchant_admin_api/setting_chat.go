8bf07cdc93c869a61030406df4ecd28788ea74fe
jax <627137323@qq.com>
2021-01-11 20:13:16 +0800

    no message

--- server/controller/merchant_admin_api/setting_chat.go
+++ server/controller/merchant_admin_api/setting_chat.go
@@ -1,6 +1,11 @@
 package merchant_admin_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/global"
+	libs_http "api/libs/http"
+	"api/models"
+	"github.com/gin-gonic/gin"
+)
 
 type SettingChat struct {
 }
@@ -9,11 +14,63 @@ func (c *SettingChat) Get(ctx *gin.Context) {
 
 }
 func (c *SettingChat) Create(ctx *gin.Context) {
-
+	var params struct {
+		AssignRule          int
+		IsAssignLastService bool
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	settingChat := &models.SettingChats{
+		AssignRule:          &params.AssignRule,
+		IsAssignLastService: &params.IsAssignLastService,
+	}
+	res := global.DBMaster.Table(settingChat.GetTableName()).Create(settingChat)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "添加成功")
+	return
 }
 func (c *SettingChat) Update(ctx *gin.Context) {
-
+	var params struct {
+		Id                  uint64
+		AssignRule          int
+		IsAssignLastService bool
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	var settingChat models.SettingChats
+	settingChat.Id = &params.Id
+	settingChat.IsAssignLastService = &params.IsAssignLastService
+	settingChat.AssignRule = &params.AssignRule
+	res := global.DBMaster.Table(settingChat.GetTableName()).Update(&settingChat)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "修改成功")
+	return
 }
 func (c *SettingChat) Delete(ctx *gin.Context) {
-
+	var params struct {
+		Id uint64
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	var settingChat models.SettingChats
+	settingChat.Id = &params.Id
+	res := global.DBMaster.Table(settingChat.GetTableName()).Delete(&settingChat)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "删除成功")
+	return
 }
--- server/controller/merchant_admin_api/setting_robot.go
+++ server/controller/merchant_admin_api/setting_robot.go
@@ -1,6 +1,11 @@
 package merchant_admin_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/global"
+	libs_http "api/libs/http"
+	"api/models"
+	"github.com/gin-gonic/gin"
+)
 
 type SettingRobot struct {
 }
@@ -9,11 +14,69 @@ func (m *SettingRobot) Get(ctx *gin.Context) {
 
 }
 func (m *SettingRobot) Create(ctx *gin.Context) {
-
+	var params struct {
+		AutoClose    uint
+		AfterAskTime uint
+		WaitAsk      uint
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	settingRobots := &models.SettingRobots{
+		AutoClose:    &params.AutoClose,
+		AfterAskTime: &params.AfterAskTime,
+		WaitAsk:      &params.WaitAsk,
+	}
+	res := global.DBMaster.Table(settingRobots.GetTableName()).Create(settingRobots)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "添加成功")
+	return
 }
 func (m *SettingRobot) Update(ctx *gin.Context) {
-
+	var params struct {
+		Id           uint64
+		AutoClose    uint
+		AfterAskTime uint
+		WaitAsk      uint
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	settingRobots := &models.SettingRobots{
+		Model:        models.Model{Id: &params.Id},
+		AutoClose:    &params.AutoClose,
+		AfterAskTime: &params.AfterAskTime,
+		WaitAsk:      &params.WaitAsk,
+	}
+	res := global.DBMaster.Table(settingRobots.GetTableName()).Update(settingRobots)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "修改成功")
+	return
 }
 func (m *SettingRobot) Delete(ctx *gin.Context) {
-
+	var params struct {
+		Id uint64
+	}
+	if err := ctx.BindJSON(&params); err != nil {
+		libs_http.RspState(ctx, 1, err)
+		return
+	}
+	settingRobots := &models.SettingRobots{
+		Model: models.Model{Id: &params.Id},
+	}
+	res := global.DBMaster.Table(settingRobots.GetTableName()).Delete(settingRobots)
+	if res.Error != nil {
+		libs_http.RspState(ctx, 1, res.Error)
+		return
+	}
+	libs_http.RspState(ctx, 0, "删除成功")
+	return
 }
